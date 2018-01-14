8f2ffa3c942ef41d98da3a0828fd8fc9a8f23c17
mazbah932 <33983949+mazbah932@users.noreply.github.com>
2018-01-14 17:00:59 +0600

    Add files via upload

---
+++ back-to-topBtn.js
@@ -0,0 +1,18 @@
+/*Scroll to top when arrow up clicked BEGIN Back to Top buttom*/
+    $(window).scroll(function() {
+        var height = $(window).scrollTop();
+        if (height > 100) {
+            $('#back2Top').fadeIn();
+        } else {
+            $('#back2Top').fadeOut();
+        }
+    });
+    $(document).ready(function() {
+        $("#back2Top").click(function(event) {
+            event.preventDefault();
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+            return false;
+        });
+
+    });
+// Scroll to top when arrow up clicked Back to top bottom END
